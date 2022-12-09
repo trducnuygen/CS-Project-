c2d3317a90ef42561fb3f23402cd9631a970d914
Duc Nguyen <nguyenminhduc890@gmail.com>
2022-12-09 18:35:04 +0700

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -5,6 +5,7 @@ import zipfile
 od.download(
     'https://www.kaggle.com/datasets/jessicali9530/celeba-dataset', force=True)
 
+# unzip file
 Dataset = "celeba-dataset"
 with zipfile.ZipFile("./celeba-dataset/"+Dataset+".zip", "r") as z:
     z.extractall(".")
